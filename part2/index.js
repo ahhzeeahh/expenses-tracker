191e111633e0a3cda875f51825454dbfabcb335e
@ahhzeeahh <140858710+ahhzeeahh@users.noreply.github.com>
2025-04-25 11:11:45 -0500

    Update index.js

--- part2/index.js
+++ part2/index.js
@@ -4,12 +4,28 @@ const submitBtn = document.getElementById("submit");
 const savePrint = document.getElementById("print");
 let rowEl = document.getElementById("body");
 let finalNum = document.getElementById("final")
-
 let formData = [];
 
+
+function masterFunction(e) {
+    e.preventDefault()
+
+    //step 1
+    validateForm()
+    // step 2
+    createObj()
+    //step 3
+    addToTable()
+     
+
+    let savedInt = Number(formEl[1].value)
+    doMath(savedInt, finalNum)
+    formEl.reset()
+}
+
 function validateForm() {
-    //its better to use individual form ID's instead of input if you are gonna use array method
-        if (formEl[0].value == "" || formEl[1].value == "") {
+   //read MND dock to replace this checkValidity
+    if (formEl[0].value == "" || formEl[1].value == "") {
             alert("Please fill out required form entries.")
             formEl[0].classList.add("error");
             formEl[1].classList.add("error");  
@@ -17,15 +33,11 @@ function validateForm() {
 
             formEl[0].classList.remove("error");
             formEl[1].classList.remove("error");           
-        }
+        }     
       
 }
+function createObj() {
 
-function addToTable(e) {
-
-   e.preventDefault()
-
-   //create a arsonal of data, potential turn it into JSON for fun idkkkkk
    const dataEntered = { 
     title: formEl[0].value, 
     money: formEl[1].value, 
@@ -33,70 +45,59 @@ function addToTable(e) {
     };
 
     formData.push(dataEntered)
-
-    //doing this to practice with table methods and get way from innerHTML its still pretty longwinded
-   let newRow = rowEl.insertRow(-1)
-   let cell1 = newRow.insertCell(0);    cell1.textContent = dataEntered.title
-   let cell2 = newRow.insertCell(1);    cell2.textContent = dataEntered.money
-   let cell3 = newRow.insertCell(2);    cell3.textContent = dataEntered.date
-   let cell4 = newRow.insertCell(3);    cell4.innerHTML = "<button>Delete?</button>";
-
-
-    var savedInt = Number(dataEntered.money)
-
-    //this stuff needs to be updated after every entry
-   document.querySelectorAll("#my-form input").forEach(input => {input.value = '' });
-
-   doMath(savedInt)
-
-
-}
-
-function doMath(savedInt) {
-
-//let allNumbers = document.querySelectorAll("#output-data tr td:nth-child(2)")
-console.log(savedInt + " is the number to be added/ delted bases on its positie or negitive status")
-console.log(Number(finalNum.textContent) + " + " + savedInt)
-   let newNum = Number(finalNum.textContent) + savedInt;
-   
-   finalNum.textContent = newNum
-
-
+    console.log(formData)
     
 }
 
+function addToTable() {
+   let indexArr = formData.length - 1
+   let newRow = rowEl.insertRow(-1)
+   let cell1 = newRow.insertCell(0);    
+   let cell2 = newRow.insertCell(1);    
+   let cell3 = newRow.insertCell(2);  
+   let cell4 = newRow.insertCell(3);  
+     
+   cell1.textContent = formData[indexArr].title
+   cell2.textContent = formData[indexArr].money
+   cell3.textContent = formData[indexArr].date
+   cell4.innerHTML = "<button>Delete?</button>";
 
-submitBtn.addEventListener('click', validateForm)
-formEl.addEventListener('submit', addToTable)
+
+}
+
+function doMath(savedInt, finalNum) {
+    
+   let newNum = Number(finalNum.textContent) + savedInt;  
+   finalNum.textContent = newNum
+    
+}
+
+ formEl.addEventListener('submit', masterFunction)
 savePrint.addEventListener('click', function () {
     window.print()
     JSON.stringify(dataEntered)
-
 })
+
+
+
 rowEl.addEventListener("click", function(e) {
+
+    
     if (e.target.matches("td > button")) {
                 let btn = e.target
-                let thatBtnRow= btn.closest('tr')
+                let thatBtnRow = btn.closest('tr')
+                let i =  Number(thatBtnRow.rowIndex) - 1
                 let amtToDelete = thatBtnRow.children[1].textContent
                 let savedInt = Number(amtToDelete) * -1
-                doMath(savedInt)
-                thatBtnRow.remove(btn)
-    }
-});
 
-/*
-function updateButtonListeners() {
-    //im unsure if below is the best way to get ALL the numbers dynamically each time theres a change in the DOM 
-    let allNumbers = document.querySelectorAll("#output-data tr td:nth-child(2)")
-    let allButtons = document.querySelectorAll("td > button")
-    allButtons.forEach(btn => {
-        
-            btn.addEventListener("click", function () {
-    
-    
-                })
-    
-        });
-    
+
+                doMath(savedInt, finalNum)
+                formData.splice(i, 1)
+                thatBtnRow.remove(btn)
+
     }
-*/
+
+
+
+    
+});
